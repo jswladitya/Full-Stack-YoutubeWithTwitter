f1a2669362798b45b4c645bbf017558458547606
jswladitya <jswladitya0403@gmail.com>
2024-09-11 18:43:21 +0530

    file upload on cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+// fs : to manage file system
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+//ek method bana lete he usme aap mujhe us localfile ka path doge , wo method us file ko upload kardega & ager successfully file upload ho gaya to file to file ko unlink kar dunga
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+            // it can be image, raw, video : "auto" detect by yourself
+        })
+        // file has been uploaded successfully
+        //console.log("file is uploaded on cloudinary ", response.url); //upload hone ke baad public url milega
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary}
