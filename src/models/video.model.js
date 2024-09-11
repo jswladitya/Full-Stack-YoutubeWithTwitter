5e3a6bb5b8d81c6ca0a383f54ea0e80d0a82a74b
jswladitya <jswladitya0403@gmail.com>
2024-09-11 17:43:04 +0530

    add user , video model , jwt

---
+++ src/models/video.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
+
+
+
+
+//NOTE: since we gonna store all videos viewed by user at watchHistory , it makes it complex so we need to install npm package "mongoose-aggregate-paginate" to write mongoDB aggregation queries
